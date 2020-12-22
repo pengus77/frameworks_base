b474d111bdafef253f2668308fcbfac7cb4807f0
Pietro Ivan Cerrato <ivan@cerrato.biz>
2020-12-22 14:40:29 +0100

    themes: add MonteCarlo accent color

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -30,6 +30,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorAmethystOverlay \
 	AccentColorAquamarineOverlay \
 	AccentColorTangerineOverlay \
+	AccentColorMonteCarloOverlay \
 	FontNotoSerifSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
