8e017b86fc79bbd6f28921f203ab2ce2dddbbad4
Pietro Ivan Cerrato <ivan@cerrato.biz>
2021-01-07 18:56:58 +0100

    PixelPropsUtils: switch back to Build.FINGERPRINT instead of Build.DATE

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -84,7 +84,7 @@ public class PixelPropsUtils {
         }
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")){
-            setPropValue("FINGERPRINT", Build.DATE);
+            setPropValue("FINGERPRINT", Build.FINGERPRINT);
         }
     }
 
